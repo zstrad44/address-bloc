adee351627e013ffc533b05e30f383f0c2033feb
zstrad44 <zachstradling@gmail.com>
2016-11-27 15:21:48 -0700

    completed assignment 5

--- models/address_book.rb
+++ models/address_book.rb
@@ -6,7 +6,7 @@ require_relative 'entry'
      def initialize
        @entries = []
      end
- 
+
 
    def add_entry(name, phone_number, email)
      # #9
@@ -20,5 +20,19 @@ require_relative 'entry'
      end
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
+   end
+
+   # Remove entry method
+
+   def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && entry.phone_number && entry.email
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)  
    end
  end
