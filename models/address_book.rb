b46e22e097e3c62c6c481d3101106129d9125dfb
zstrad44 <zachstradling@gmail.com>
2016-11-30 21:45:37 -0700

    Completed assignment 8

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,63 +1,63 @@
 require_relative 'entry'
 require "csv"
 
-   class AddressBook
+  class AddressBook
      attr_reader :entries
 
      def initialize
        @entries = []
      end
 
-
-   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
+     def add_entry(name, phone_number, email)
+       # #9
+       index = 0
+       entries.each do |entry|
+       # #10
+         if name < entry.name
+           break
+         end
+         index+= 1
        end
-       index+= 1
+       # #11
+       entries.insert(index, Entry.new(name, phone_number, email))
      end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
 
-   def import_from_csv(file_name)
-      # Implementation goes here
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-   # #8
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     def import_from_csv(file_name)
+       csv_text = File.read(file_name)
+       csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+       csv.each do |row|
+         row_hash = row.to_hash
+         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+       end
      end
-   end
 
-   def binary_search(name)
-     # #1
-    lower = 0
-    upper = entries.length - 1
-
-    # #2
-    while lower <= upper
-      # #3
-      mid = (lower + upper) / 2
-      mid_name = entries[mid].name
-
-      # #4
-      if name == mid_name
-        return entries[mid]
-      elsif name < mid_name
-        upper = mid - 1
-      elsif name > mid_name
-        lower = mid + 1
+     def binary_search(name)
+      lower = 0
+      upper = entries.length - 1
+      while lower <= upper
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+        if name == mid_name
+          return entries[mid]
+        elsif name < mid_name
+          upper = mid - 1
+        elsif name > mid_name
+          lower = mid + 1
+        end
       end
+      return nil
     end
 
-    # #5
-    return nil
+     def iterative_search(name)
+      entries.each do |entry|
+        if entry.name == name
+          return entry
+            break
+        else
+          nil
+        end
+      end
+        nil
+    end
 
-   end
-
- end
+  end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -25,20 +25,20 @@ require_relative '../models/address_book'
      end
    end
 
-      describe "#add_entry" do
-        it "adds only one entry to the address book" do
-          book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-          expect(book.entries.size).to eq(1)
-        end
-
-        it "adds the correct information to entries" do
-          book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-          new_entry = book.entries[0]
-          expect(new_entry.name).to eq('Ada Lovelace')
-          expect(new_entry.phone_number).to eq('010.012.1815')
-          expect(new_entry.email).to eq('augusta.king@lovelace.com')
-        end
+    describe "#add_entry" do
+      it "adds only one entry to the address book" do
+        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        expect(book.entries.size).to eq(1)
       end
+
+      it "adds the correct information to entries" do
+        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        new_entry = book.entries[0]
+        expect(new_entry.name).to eq('Ada Lovelace')
+        expect(new_entry.phone_number).to eq('010.012.1815')
+        expect(new_entry.email).to eq('augusta.king@lovelace.com')
+      end
+    end
 
        # Test that AddressBook's .import_from_csv() method is working as expected
        describe "#import_from_csv" do
@@ -51,41 +51,41 @@ require_relative '../models/address_book'
            expect(book_size).to eq 5
          end
 
+         it "imports the 1st entry" do
+           book.import_from_csv("entries.csv")
+           #check the first entry
+           entry_one = book.entries[0]
+           check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+         end
 
-     it "imports the 1st entry" do
-       book.import_from_csv("entries.csv")
-       #check the first entry
-       entry_one = book.entries[0]
-       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-     it "imports the 2nd entry" do
-       book.import_from_csv("entries.csv")
-       # Check the second entry
-       entry_two = book.entries[1]
-       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
-     end
+         it "imports the 2nd entry" do
+           book.import_from_csv("entries.csv")
+           # Check the second entry
+           entry_two = book.entries[1]
+           check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+         end
 
-     it "imports the 3rd entry" do
-       book.import_from_csv("entries.csv")
-       # Check the third entry
-       entry_three = book.entries[2]
-       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
-     end
+         it "imports the 3rd entry" do
+           book.import_from_csv("entries.csv")
+           # Check the third entry
+           entry_three = book.entries[2]
+           check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+         end
 
-     it "imports the 4th entry" do
-       book.import_from_csv("entries.csv")
-       # Check the fourth entry
-       entry_four = book.entries[3]
-       check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
-     end
+         it "imports the 4th entry" do
+           book.import_from_csv("entries.csv")
+           # Check the fourth entry
+           entry_four = book.entries[3]
+           check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+         end
 
-     it "imports the 5th entry" do
-       book.import_from_csv("entries.csv")
-       # Check the fifth entry
-       entry_five = book.entries[4]
-       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
-     end
-   end
+         it "imports the 5th entry" do
+           book.import_from_csv("entries.csv")
+           # Check the fifth entry
+           entry_five = book.entries[4]
+           check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+         end
+       end
 
    describe "#binary_search" do
      it "searches AddressBook for a non-existent entry" do
@@ -137,4 +137,56 @@ require_relative '../models/address_book'
 
    end
 
-end
+   # Assignmnet 8 ////////////////////////
+   # /////////////////////////////////////
+
+   describe "#iterative_search" do
+     it "searches AddressBook for a non-existent entry" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Dan")
+       expect(entry).to be_nil
+     end
+
+     it "searches AddressBook for Bill" do
+      book.import_from_csv("entries.csv")
+      entry = book.iterative_search("Bill")
+      expect(entry).to be_a Entry
+      check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "searches AddressBook for Bob" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Bob")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+
+     it "searches AddressBook for Joe" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Joe")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
+
+     it "searches AddressBook for Sally" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Sally")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Sally", "555-555-4646", "sally@blocmail.com")
+     end
+
+     it "searches AddressBook for Sussie" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Sussie")
+       expect(entry).to be_a Entry
+       check_entry(entry, "Sussie", "555-555-2036", "sussie@blocmail.com")
+     end
+
+     it "searches AddressBook for Billy" do
+       book.import_from_csv("entries.csv")
+       entry = book.iterative_search("Billy")
+       expect(entry).to be_nil
+     end
+   end
+
+ end
